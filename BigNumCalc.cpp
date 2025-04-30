302e59516a2eb39195d29f1671412dd1da09d657
1850837 <a1850837@adelaide.edu.au>
2025-04-30 16:17:33 +0930

    add

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -13,7 +13,57 @@ std::list<int> BigNumCalc::buildBigNum(std::string numString){
 }
 
 std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2){
+    int remainder = 0;
+    int tempResult = 0;
+    std::list<int> result;
+    int size;
+    auto num1Ptr = num1.end();
+    std::advance(num1Ptr, -1);
+    auto num2Ptr = num2.end();
+    std::advance(num2Ptr, -1);
 
+    //determining size for loop
+    if (num1.size() > num2.size()){
+        size = num1.size() + 1;
+    }
+    else {
+        size = num2.size() + 1;
+    }
+
+    for (int i = 0; i < size; i++){
+        //adding the three numbers together
+        tempResult = *num1Ptr + *num2Ptr + remainder;
+
+        //pushing front the last digit of remainder
+        result.push_front(tempResult%10);
+
+        //getting the remainder
+        remainder = (tempResult/10)%10;
+
+        //iterating the num1Ptr
+        if (i < num1.size() - 1){
+            std::advance(num1Ptr, -1);
+        }
+        else if (i == num1.size() - 1){
+            *num1Ptr = 0;
+        }
+
+        //iterating the num2Ptr
+        if (i < num2.size() - 1){
+            std::advance(num2Ptr, -1);
+        }
+        else if (i == num2.size() - 1){
+            *num2Ptr = 0;
+        }
+    }
+
+    //ensure there are no leading 0's
+    while (result.front() == 0){
+        result.pop_front();
+    }
+
+    return result;
+    
 }
 
 std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2){
--- main.cpp
+++ main.cpp
@@ -5,12 +5,24 @@ int main(){
 
     BigNumCalc a;
 
-    std::list<int> b = a.buildBigNum("300076895");
-    auto b_front = b.begin();
+    // std::list<int> b = a.buildBigNum("300076895");
+    // auto b_front = b.begin();
 
-    for (int i = 0; i < b.size(); i++){
-        std::cout << *b_front << " ";
-        std::advance(b_front, 1);
+    // for (int i = 0; i < b.size(); i++){
+    //     std::cout << *b_front << " ";
+    //     std::advance(b_front, 1);
+    // }
+    // std::cout << "\n";
+
+    std::list<int> num1 = a.buildBigNum("52983741982");
+    std::list<int> num2 = a.buildBigNum("238974120");
+
+    std::list<int> result = a.add(num1, num2);
+    auto resultFront = result.begin();
+
+    for (int i = 0; i < result.size(); i++){
+        std::cout << *resultFront;
+        std::advance(resultFront, 1);
     }
     std::cout << "\n";
 
